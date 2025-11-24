7a728057889721a78a3f0c452da91c90dd5dd13b
xiaochen-n8n <yoloxx0.0xx@gmail.com>
2025-11-24 22:37:43 +0800

    新增账户和昵称

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,11 +1,22 @@
 import json
-import random
 from channels.generic.websocket import AsyncWebsocketConsumer
+from asgiref.sync import sync_to_async
+from .models import Profile
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        if not self.scope["user"].is_authenticated:
+            await self.close()
+            return
+
+        def get_display_name(user):
+            try:
+                return Profile.objects.get(user=user).nickname
+            except Profile.DoesNotExist:
+                return user.username
+
         self.room_group_name = "chat_room"
-        self.username = f"User{random.randint(1000, 9999)}"
+        self.username = await sync_to_async(get_display_name)(self.scope["user"])
 
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
@@ -17,7 +28,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         data = json.loads(text_data)
         message = data["message"]
-        # 广播到 Redis group
         await self.channel_layer.group_send(
             self.room_group_name,
             {"type": "chat.message", "username": self.username, "message": message},
